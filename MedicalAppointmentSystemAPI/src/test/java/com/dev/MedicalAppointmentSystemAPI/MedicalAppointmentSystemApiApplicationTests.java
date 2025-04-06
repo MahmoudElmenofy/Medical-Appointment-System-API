12f91b9cdf00c51dad9229ae379da2b9057810b2
Mahmoud Elmenofy <elmenofy45@gmail.com>
2025-04-06 21:42:56 +0200

    Add files via upload

---
+++ MedicalAppointmentSystemAPI/src/test/java/com/dev/MedicalAppointmentSystemAPI/MedicalAppointmentSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.MedicalAppointmentSystemAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalAppointmentSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
